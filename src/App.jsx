8b106689edde1251a78394ed090fa7c8253d9c3d
Prataya <pratayasilla@gmail.com>
2026-03-07 04:38:55 +0530

    Loading screen disabled

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
-import { useState } from 'react';
 import './App.css';
-import PageLoader from './components/PageLoader/PageLoader';
 
 function App({children}) {
-  const [isLoading, setIsLoading] = useState(true);
-
-  const handleLoadComplete = () => {
-    setIsLoading(false);
-  };
-
   return (
     <>
-      {isLoading && <PageLoader onLoadComplete={handleLoadComplete} />}
       {children}
     </>
   )
